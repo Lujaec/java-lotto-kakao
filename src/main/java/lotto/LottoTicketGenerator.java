e42c93d95ac8d5b568f6e5c43accccadf6719d5e
revan.lib <revan.lib@kakaocorp.com>
2026-02-10 13:23:33 +0900

    feat: LottoTicketGenerator 구현

--- src/main/java/lotto/LottoTicketGenerator.java
+++ src/main/java/lotto/LottoTicketGenerator.java
@@ -1,4 +1,17 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoTicketGenerator {
+	public static List<LottoTicket> generate(int ticketNumber) {
+		if (ticketNumber <= 0)
+			throw new IllegalArgumentException("티켓의 개수는 1이상이어야 합니다");
+
+		List<LottoTicket> ret = new ArrayList<>();
+		for(int i = 0; i < ticketNumber; i++){
+			ret.add(new LottoTicket());
+		}
+		return ret;
+	}
 }
--- src/test/java/lotto/LottoTicketGeneratorTest.java
+++ src/test/java/lotto/LottoTicketGeneratorTest.java
@@ -1,14 +1,27 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LottoTicketGenerator {
+public class LottoTicketGeneratorTest {
 	@Test
-	@DisplayName("n개의 티켓을 생성해서 List로 반환한다")
+	@DisplayName("양의 정수 n개의 티켓을 생성해서 List로 반환한다")
 	public void generate(){
 		final int TICKET_NUMBER = 17;
+		List<LottoTicket> lottoTickets = LottoTicketGenerator.generate(TICKET_NUMBER);
+		assertThat(lottoTickets.size()).isEqualTo(TICKET_NUMBER);
+	}
 
-		LottoTicketGenerator lottoTicketGenerator = new LottoTicketGenerator(TICKET_NUMBER);
+	@Test
+	@DisplayName("양이 아닌 정수 n개의 티켓을 생성하려고 하면 에러를 발생")
+	public void generate_not_positive(){
+		final int TICKET_NUMBER = 0;
+		assertThatThrownBy(() -> LottoTicketGenerator.generate(TICKET_NUMBER))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
