2f9d7d7b99507ec2de359a5e6da02f68c1a961c0
revan.lib <revan.lib@kakaocorp.com>
2026-02-10 15:17:26 +0900

    test: 랭크별 횟수 조회 메서드 검증

--- src/test/java/lotto/LottoStatisticsTest.java
+++ src/test/java/lotto/LottoStatisticsTest.java
@@ -47,4 +47,33 @@ public class LottoStatisticsTest {
 		assertThat(lottoStatistics.profitRate()).isEqualTo(0);
 		assertThat(lottoStatistics.totalPrizeMoney()).isEqualTo(0);
 	}
+
+	@Test
+	@DisplayName("countOf는 등수별 누적 개수를 반환한다")
+	public void count_of_rank() {
+		LottoStatistics lottoStatistics = new LottoStatistics();
+		List<Rank> ranks = List.of(
+			Rank.FIFTH, Rank.FIFTH, Rank.FIFTH,
+			Rank.FOURTH, Rank.FOURTH,
+			Rank.OTHER
+		);
+
+		for (Rank rank : ranks) {
+			lottoStatistics.add(rank);
+		}
+
+		assertThat(lottoStatistics.countOf(Rank.FIFTH)).isEqualTo(3);
+		assertThat(lottoStatistics.countOf(Rank.FOURTH)).isEqualTo(2);
+		assertThat(lottoStatistics.countOf(Rank.OTHER)).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("countOf는 없는 등수를 조회하면 0을 반환한다")
+	public void count_of_missing_rank() {
+		LottoStatistics lottoStatistics = new LottoStatistics();
+		lottoStatistics.add(Rank.FIFTH);
+		lottoStatistics.add(Rank.OTHER);
+
+		assertThat(lottoStatistics.countOf(Rank.FIRST)).isEqualTo(0);
+	}
 }
