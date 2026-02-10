1bea056617a7ee9fb3724efc7f3e2ed3cf8a725d
revan.lib <revan.lib@kakaocorp.com>
2026-02-10 10:37:27 +0900

    feat: Rank 및 RankTest 추가

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public enum Rank {
+	FIRST(2000000000L),
+	SECOND(30000000L),
+	THIRD(1500000L),
+	FOURTH(50000L),
+	FIFTH(5000L),
+	;
+	private final long prizeMoney;
+
+	Rank(long prizeMoney) {
+		this.prizeMoney = prizeMoney;
+	}
+
+	public long prizeMoney(){
+			return prizeMoney;
+		}
+}
---
+++ src/test/java/lotto/RankTest.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RankTest {
+	private final Map<String, Long> mapper = new HashMap<>() {{
+		put("FIRST", 2000000000L);
+		put("SECOND", 30000000L);
+		put("THIRD", 1500000L);
+		put("FOURTH", 50000L);
+		put("FIFTH", 5000L);
+	}};
+
+	@Test
+	@DisplayName("ENUM 필드와 값이 일치하는지 확인")
+	public void test_enum(){
+		for (Map.Entry<String, Long> map: mapper.entrySet()){
+			Assertions.assertThat(Rank.valueOf(map.getKey()).prizeMoney()).isEqualTo(map.getValue());
+		}
+	}
+
+}
