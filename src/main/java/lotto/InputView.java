7271a40374a81aa0210b7d1e4038c0a196761948
revan.lib <revan.lib@kakaocorp.com>
2026-02-11 13:04:47 +0900

    fix: View 에서 로또 번호 비즈니스 규칙 검증 삭제

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -26,33 +26,11 @@ public class InputView {
 		ArrayList<LottoNumber> winningNumbers = new ArrayList<>();
 		StringTokenizer stringTokenizer = new StringTokenizer(input, ",");
 		while (stringTokenizer.hasMoreTokens()) winningNumbers.add(parseLottoNumber(stringTokenizer.nextToken()));
-		validateWinningNumbersCount(winningNumbers);
-		validateUniqueNumbers(winningNumbers);
 		return winningNumbers;
 	}
 
 	private LottoNumber parseLottoNumber(String token) {
 		return new LottoNumber(parseInteger(token));
-	}
-
-	private void validateWinningNumbersCount(ArrayList<LottoNumber> winningNumbers) {
-		if (winningNumbers.size() != 6) {
-			throw new IllegalArgumentException("당첨 번호는 6개를 입력해야 합니다.");
-		}
-	}
-
-	private void validateUniqueNumbers(ArrayList<LottoNumber> winningNumbers) {
-		ArrayList<LottoNumber> uniqueNumbers = new ArrayList<>();
-		for (LottoNumber winningNumber : winningNumbers) {
-			validateNotDuplicated(uniqueNumbers, winningNumber);
-			uniqueNumbers.add(winningNumber);
-		}
-	}
-
-	private void validateNotDuplicated(ArrayList<LottoNumber> uniqueNumbers, LottoNumber winningNumber) {
-		if (uniqueNumbers.contains(winningNumber)) {
-			throw new IllegalArgumentException("당첨 번호는 중복될 수 없습니다.");
-		}
 	}
 
 	private int parseInteger(String input) {
